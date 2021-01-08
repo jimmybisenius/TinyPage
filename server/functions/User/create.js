7bd42b9f2857bc634c6fd0a5271487b68094e6a3
Jim Bisenius <jim@neutroncreative.com>
2021-01-07 21:10:10 -0500

    Fix custom domain bug

--- server/functions/User/create.js
+++ server/functions/User/create.js
@@ -19,8 +19,12 @@ module.exports = (req, res) => {
         email: req.body.email || null,
         password: hash,
         hash: md5(req.body.email)
-      }).save(function (err, user) {
-        new Profile({parent: user._id, handle: req.body.handle || user._id}).save(function (err, profile) {
+      }).save(async function (err, user) {
+        let profile = await new Profile({
+		parent: user._id,
+		handle: req.body.handle || user._id
+	}).save();
+	  //new Profile({parent: user._id, handle: req.body.handle || user._id}).save(function (err, profile) {
           user.active_profile = profile._id;
           user.save(function (err, user) {
             if (err) return res.send(err);
@@ -36,8 +40,7 @@ module.exports = (req, res) => {
               )
             });
           });
-        });
+        //});
       });
     });
-  });
-};
+  });};
